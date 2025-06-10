2f94736e1dc31280398b9b44b771087c377eaf1a
Manuel Cattelan <cattelanmanuel01@gmail.com>
2025-06-10 11:12:03 +0200

    feat: revisited chapter 1 solutions

--- chapter_01/exercise_24.c
+++ chapter_01/exercise_24.c
@@ -11,20 +11,12 @@ typedef enum {
 
 int main(void) {
   int c;
-  /* There is no correct default value for the previous character read,
-   * but EOF is a good representation of the fact that no valid character has
-   * been read yet. */
-  int c_prev = EOF;
-  /* We use a dedicated variable to check if we're escaping the current
-   * character instead of exploiting c_prev for a single reason: without it,
-   * we're unable to handle double consecutive backslashes, such as in
-   * "hello, world\\", in which case, without is_escaped, the last '"' character
-   * would be interpreted as an escape sequence. */
+  /* Track escape state to properly handle scenarios like consecutive
+   * backslashes. Without this flag, cases like "hello, world\\" would be
+   * mishandled and the last " character would be interpreted as escaped. */
   bool is_escaped = false;
-  /* Track if we've consumed a / which could result in a comment based on what
-   * character follows next. In case another / or a * follow, that would be the
-   * beginning of a comment, otherwise we should print the previously consumed
-   * single / and the new character. */
+  /* Track slash state for comment detection so that we can determine if a '/'
+   * or '*' character is the start of a single-line or multi-line comment. */
   bool is_comment_start = false;
   Status c_status = OUTSIDE; /* if we're inside a character constant or not */
   Status s_status = OUTSIDE; /* if we're inside a string or not */
@@ -34,15 +26,11 @@ int main(void) {
 
   while ((c = getchar()) != EOF) {
     if (c_status == INSIDE) {
-      /* If we find the closing ' character and it is not escaped, that marks
-       * the end of our character constant. */
       if (c == '\'' && !is_escaped) {
         c_status = OUTSIDE;
       }
       is_escaped = (c == '\\' && !is_escaped);
     } else if (s_status == INSIDE) {
-      /* If we find the closing " character and it is not escaped, that marks
-       * the end of our string. */
       if (c == '"' && !is_escaped) {
         s_status = OUTSIDE;
       }
@@ -54,21 +42,19 @@ int main(void) {
         s_status = INSIDE;
       } else if (c == '/') {
         if (is_comment_start) {
-          /* Wait till the end of the line so that the comment ends. */
           while ((c = getchar()) != EOF && c != '\n') {
             ;
           }
           is_comment_start = false;
         } else {
-          /* We keep track of the fact that we've encountered a / character
-           * which could result in a comment depending on the character consumed
-           * after the current one. */
           is_comment_start = true;
         }
       } else if (c == '*') {
         if (is_comment_start) {
-          /* Wait till we find the matching comment characters so that the
-           * comment ends. */
+          /* There is no correct default value for the previous character read,
+           * but EOF is a good representation of the fact that no valid
+           * character has been read yet. */
+          int c_prev = EOF;
           while ((c = getchar()) != EOF && !(c == '/' && c_prev == '*')) {
             c_prev = c;
           }
@@ -99,7 +85,6 @@ int main(void) {
         }
       }
       is_escaped = false;
-      c_prev = c;
     }
   }
 
